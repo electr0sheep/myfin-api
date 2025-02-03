56513c845a8f3d2e482aa51f35f923f4b60e8afb
Michael DeGraw <michael.degraw@flybreeze.com>
2025-02-03 16:27:46 -0700

    Support SSL

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import https from 'https';
+import fs from 'fs';
 import app from './app.js';
 import 'dotenv/config.js';
 
@@ -5,7 +7,19 @@ import 'dotenv/config.js';
 process.env.TZ = 'Etc/UTC';
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`MyFin server listening on port ${PORT}`);
-});
+
+if (process.env.SSL_CERT_PATH && process.env.SSL_KEY_PATH) {
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+  };
+
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`MyFin secure server listening on port ${PORT}`)
+  });
+} else {
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`MyFin unsecured server listening on port ${PORT}`);
+  });
+}
